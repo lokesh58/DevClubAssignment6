e4b803472e4a2f7554d0e479db4e5b6a681772fd
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-28 14:50:13 +0530

    Added login authentication basic

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/source/home.html');
 });
 
 app.get('/login', (req, res) => {
-	res.send("Login will be updated soon");
+	res.sendFile(__dirname + '/source/login.html');
+});
+
+app.post('/auth', (req, res) => {
+	console.log('Username: ' + req.body.user);
+	console.log('Password: ' + req.body.pass);
+	res.send('Working on login Auth');
 });
 
 app.get('/register', (req, res) => {
