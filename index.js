128b994368261c4bac194164c263bf21c94d650a
Lokesh Patel <patellokesh05899@gmail.com>
2017-12-29 12:17:48 +0530

    Corrected mistake in redirect

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post('/login/auth', (req, res) => {
 					} else {
 						res.cookie('loginInfo', user, {maxAge: 3.6e6});
 						res.write("Login successful!");
-						res.redirect('/' + user);
+						res.redirect('../' + user);
 					}
 				}
 			});
